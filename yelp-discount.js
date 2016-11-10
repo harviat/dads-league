37dabb979134c95a0b50839ecc88d64126931795
harviat <harviat@gmail.com>
2016-11-09 19:00:22 -0600

    Update yelp-discount.js

--- yelp-discount.js
+++ yelp-discount.js
@@ -4,4 +4,6 @@ if (document.referrer.length && reURL.test(document.referrer)) {
       window.onload = function prefill_coupon() {
 document.getElementById("id123-couponcode").value = "10OFF-900";
 }
-} 
+} else {
+    alert();
+}
