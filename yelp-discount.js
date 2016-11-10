61aad026597f2a7fe517e5a50b71222f4a2e533c
harviat <harviat@gmail.com>
2016-11-09 18:47:52 -0600

    Add files via upload

---
+++ yelp-discount.js
@@ -0,0 +1,6 @@
+﻿if (document.referrer.indexOf(‘yelp.com') != -1) {
+     //user came from yelp.com
+window.onload = function prefill_coupon() {
+document.getElementById("id123-couponcode").value = "10OFF-900";
+}
+}
